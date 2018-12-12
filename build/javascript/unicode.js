500c59fb95499ae468816c8782dec048dc4f147a
Wane Smith <wanesmith7@gmail.com>
2018-12-11 23:11:20 -0500

    file changes

--- build/javascript/unicode.js
+++ build/javascript/unicode.js
@@ -6,11 +6,13 @@ let C = String.fromCharCode(0x010C);
 let c = String.fromCharCode(0x010D);
 let a = String.fromCharCode(0x00e1);
 let A = String.fromCharCode(0x0111);
+let aa = String.fromCharCode(0x1EB1);
 let s = String.fromCharCode(0x0161);
 let d = String.fromCharCode(0x0111);
 let D = String.fromCharCode(0x0110);
 let r = String.fromCharCode(0x0159);
 let e = String.fromCharCode(0x011B);
+let ee = String.fromCharCode(0x1EC7);
 let n = String.fromCharCode(0x00F1);
 let invertedQuestionMark = String.fromCharCode(0x00BF);
 let copyrightSymbol = String.fromCharCode(0x00A9);
@@ -20,11 +22,11 @@ let registerSymbol = String.fromCharCode(0x00AE);
 
 
 unicodeCharacter.forEach(function(){
-document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n).sort();
+document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a,aa,ee,C,d,D,r,e,s,n).sort();
     
 });
 
-console.log(unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n));
+console.log(unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n,aa,ee));
 console.log(copyrightSymbol,registerSymbol,invertedQuestionMark);
 // console.log(unicodeCharacter.concat(u,c,a).sort());
 
