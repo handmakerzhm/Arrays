38cec2af5810adb16f04ef4e72b979202ccd1edf
Wane Smith <wanesmith7@gmail.com>
2018-12-11 22:37:27 -0500

    file changes

--- build/javascript/unicode.js
+++ build/javascript/unicode.js
@@ -1,15 +1,26 @@
-// let unicodeCharacter= ['s','b','a','c'];
+let unicodeCharacter= ['s','b','a','c'];
 
-// let u = String.fromCharCode(0x016E);
-// let c = String.fromCharCode(0x010D);
-// let a = String.fromCharCode(0x00e1);
+let u = String.fromCharCode(0x016F);
+let U = String.fromCharCode(0x016E);
+let C = String.fromCharCode(0x010C);
+let c = String.fromCharCode(0x010D);
+let a = String.fromCharCode(0x00e1);
+let A = String.fromCharCode(0x0111);
+let s = String.fromCharCode(0x0161);
+let d = String.fromCharCode(0x0111);
+let D = String.fromCharCode(0x0110);
+let r = String.fromCharCode(0x0159);
+let e = String.fromCharCode(0x011B);
+let n = String.fromCharCode(0x00F1);
 
-// unicodeCharacter.forEach(function(){
-// document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a).sort();
+
+
+unicodeCharacter.forEach(function(){
+document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n).sort();
     
-// });
+});
 
-// console.log(unicodeCharacter.concat(u,c,a));
+console.log(unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n));
 // console.log(unicodeCharacter.concat(u,c,a).sort());
 
 
