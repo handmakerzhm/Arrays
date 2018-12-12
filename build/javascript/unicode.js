b4bd6b2e548043b54294c926a48762a36aa6f29f
Wane Smith <wanesmith7@gmail.com>
2018-12-11 23:23:51 -0500

    file changes

--- build/javascript/unicode.js
+++ build/javascript/unicode.js
@@ -2,15 +2,18 @@ let unicodeCharacter= ['s','b','a','c'];
 
 let u = String.fromCharCode(0x016F);
 let U = String.fromCharCode(0x016E);
+let uu = String.fromCharCode(0x1EE7);
 let C = String.fromCharCode(0x010C);
 let c = String.fromCharCode(0x010D);
 let a = String.fromCharCode(0x00e1);
 let A = String.fromCharCode(0x0111);
 let aa = String.fromCharCode(0x1EB1);
+let aa2 = String.fromCharCode(0x1EAF);
 let s = String.fromCharCode(0x0161);
 let d = String.fromCharCode(0x0111);
 let D = String.fromCharCode(0x0110);
 let r = String.fromCharCode(0x0159);
+let o = String.fromCharCode(0x1ECF);
 let e = String.fromCharCode(0x011B);
 let ee = String.fromCharCode(0x1EC7);
 let n = String.fromCharCode(0x00F1);
@@ -22,11 +25,11 @@ let registerSymbol = String.fromCharCode(0x00AE);
 
 
 unicodeCharacter.forEach(function(){
-document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a,aa,ee,C,d,D,r,e,s,n).sort();
+document.getElementById('output').innerHTML = unicodeCharacter.concat(u,c,a,aa,aa2,ee,C,d,D,r,e,s,n,o).sort();
     
 });
 
-console.log(unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n,aa,ee));
+console.log(unicodeCharacter.concat(u,c,a,C,d,D,r,e,s,n,aa,aa2,ee,uu,o));
 console.log(copyrightSymbol,registerSymbol,invertedQuestionMark);
 // console.log(unicodeCharacter.concat(u,c,a).sort());
 
